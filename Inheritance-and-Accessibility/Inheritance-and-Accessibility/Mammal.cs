7c6597dadd03bb1c0c570244e017eb287582a93c
Eming44 <eming44@abv.bg>
2019-05-08 12:07:37 +0300

    Exercise inheritance

---
+++ Inheritance-and-Accessibility/Inheritance-and-Accessibility/Mammal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance_and_Accessibility
+{
+    class Mammal : Creature
+    {
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        public Mammal(string name, int age) : base(name)
+        {
+            this.Age = age;
+        }
+
+        public void Sleep()
+        {
+            Console.WriteLine("Shhh! {0} is sleeping!", this.Name);
+        }
+    }
+}
